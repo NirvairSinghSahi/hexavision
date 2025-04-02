d3e9d919e0311dfe10b0fd44a9394fe72b65fc68
KhantTiKyiSectionD <N01709727@humber.ca>
2025-04-02 14:21:09 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css'; // optional
+import App from './Kyi/App';
+import './Kyi/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
